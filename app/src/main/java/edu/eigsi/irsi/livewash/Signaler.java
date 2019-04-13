bf45d3b3dcbe22cd352a10e0e1e0664179dd3235
LoisGueret <lois.gueret.22@eigsi.fr>
2019-04-13 13:33:40 +0200

    icon+string

--- app/src/main/java/edu/eigsi/irsi/livewash/Signaler.java
+++ app/src/main/java/edu/eigsi/irsi/livewash/Signaler.java
@@ -1,11 +1,14 @@
 package edu.eigsi.irsi.livewash;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class Signaler extends AppCompatActivity {
@@ -13,6 +16,8 @@ public class Signaler extends AppCompatActivity {
     private Button buttonRmach1;
     private Button buttonEnvoyer;
     private Signaler activity;
+    private RadioButton buttonAutre;
+    private EditText editText;
 
 
     @Override
@@ -27,9 +32,27 @@ public class Signaler extends AppCompatActivity {
         buttonRmach1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent retourmach1 = new Intent(getApplicationContext(), Machine.class);
+                Intent retourmach1 = new Intent(getApplicationContext(), Machine1.class);
                 startActivity(retourmach1);
                 finish();
+                    }
+                });
+
+        this.buttonAutre = (RadioButton) findViewById(R.id.radioButtonAutre);
+        this.editText = (EditText) findViewById(R.id.editTextAutre);
+        editText.setVisibility(View.INVISIBLE);
+
+        buttonAutre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setVisibility( View.VISIBLE);
+            }
+        });
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.clearFocus();
             }
         });
 
@@ -39,11 +62,20 @@ public class Signaler extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder myPopUp = new AlertDialog.Builder(activity);
-                myPopUp.setMessage("Le signalement a bien été pris en compte. Nous vous remercions pour votre contribution.");
+                myPopUp.setMessage("Le signalement a bien été pris en compte, nous vous remercions.");
+                myPopUp.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent retourmach1 = new Intent(getApplicationContext(), Laverie.class);
+                        startActivity(retourmach1);
+                        finish();
+                    }
+                });
                 myPopUp.show();
 
             }
         });
     }
 
-}
+    }
+
